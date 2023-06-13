136020bf8b00d743014f51bd0ca3d61c7dd8ff8b
Bwandrius <122626174+Bwandrius@users.noreply.github.com>
2023-06-13 12:02:31 +0300

    wip

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,23 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Ada Lovelace' }
+    { 
+      name: 'Arto Hellas',
+      number: 869111111
+    },
+    { 
+      name: 'Ada Lovelace',
+      number: 869122222
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState(0)
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      // number: newNumber
     }
     if (persons.find(person => person.name === personObject.name)) {
       setNewName('')
@@ -24,6 +32,9 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
   return (
     <div>
@@ -33,12 +44,15 @@ const App = () => {
           name: <input value={newName} onChange={handlePersonChange}/>
         </div>
         <div>
+          number: <input value={newName} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => 
-        <p key={person.name}>{person.name}</p>  
+        <p key={person.name}>{person.name} - {person.number}</p>  
       )}
     </div>
   )
